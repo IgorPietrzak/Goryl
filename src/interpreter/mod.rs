91412726de850b4bbb4d093511335fc6ce8e3285
IgorPietrzak <igor.pietrzak70@gmail.com>
2024-08-25 21:11:53 +0100

    first ever code to be run. Interpreter done up to unary expressions and added traits to Value enum to make working with binary expressions nice in the interpreter.

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,6 +1,10 @@
+mod value;
+use crate::ast::expressions::Expr;
 use crate::ast::parser::Parser;
 use crate::syntax::scanner::Scanner;
+use crate::syntax::token::{Literal, TokenType};
 use std::io;
+use value::Value;
 
 pub fn run_file(file: String) {
     let mut scanner = Scanner::new(file);
@@ -25,4 +29,36 @@ pub fn run_prompt() {
             .expect("Could not read line");
         run_line(line);
     }
+}
+
+pub fn interpret(expr: Expr) -> Value {
+    match expr {
+        Expr::Literal(literal) => match literal.value {
+            Literal::String(s) => Value::String(s),
+            Literal::Number(n) => Value::Number(n),
+            Literal::Bool(b) => Value::Bool(b),
+            Literal::None => Value::None,
+        },
+        Expr::Grouping(grouping) => evaluate(*grouping.expression), // need to dereference with * as grouping.expression is inside a Box<T> smart pointer and we pass by value into evaluate.
+        Expr::Unary(unary) => {
+            let right = evaluate(*unary.right);
+            match unary.operator.token_type {
+                TokenType::Minus => {
+                    if let Some(num) = -right.clone() {
+                        return num;
+                    } else {
+                        return right; // runtime error
+                    }
+                }
+                TokenType::Bang => !right,
+                _ => return right,
+            }
+        }
+        Expr::Binary(binary) => return Value::None,
+    }
+}
+
+// pass it back to interpret (use for recursion) usually pass in nested sub expression.
+fn evaluate(expr: Expr) -> Value {
+    interpret(expr)
 }
---
+++ src/interpreter/value.rs
@@ -0,0 +1,108 @@
+use std::ops::{Add, Div, Mul, Neg, Not, Sub};
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    String(String),
+    Number(f64),
+    Bool(bool),
+    None,
+}
+
+// unary operators:
+
+impl Neg for Value {
+    type Output = Option<Self>;
+    fn neg(self) -> Self::Output {
+        match self {
+            Value::String(_) => None,
+            Value::Number(n) => Some(Value::Number(-n)),
+            Value::Bool(_) => None,
+            Value::None => None,
+        }
+    }
+}
+
+impl Not for Value {
+    type Output = Self;
+    fn not(self) -> Self::Output {
+        match self {
+            Value::String(s) => Value::Bool(!s.is_empty()),
+            Value::Number(n) => match n {
+                0.0 => Value::Bool(true),
+                _ => Value::Bool(false),
+            },
+            Value::Bool(bool) => Value::Bool(!bool),
+            Value::None => Value::Bool(true),
+        }
+    }
+}
+
+// binary operators:
+
+impl Add for Value {
+    type Output = Option<Self>;
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::String(s1), Value::String(s2)) => Some(Value::String(s1 + &s2)), // In Rust, the + operator for strings works by taking ownership of the left-hand side (s1 in this case) and borrowing the right-hand side (s2). This is why you need to borrow s2 using &s2, but you don't need to do this for s1.
+            (Value::Number(n1), Value::Number(n2)) => Some(Value::Number(n1 + n2)),
+            _ => None,
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = Option<Self>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(n1), Value::Number(n2)) => Some(Value::Number(n1 - n2)), // We cannot do strings because think about when they are disjoint.
+            _ => None,
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Option<Self>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(n1), Value::Number(n2)) => Some(Value::Number(n1 * n2)),
+            _ => None,
+        }
+    }
+}
+
+impl Div for Value {
+    type Output = Option<Self>;
+    fn div(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(n1), Value::Number(n2)) => {
+                if n2 == 0.0 {
+                    return None;
+                } else {
+                    return Some(Value::Number(n1 / n2));
+                }
+            }
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Value;
+    #[test]
+    fn test_neg() {
+        let value = Value::Number(5.0);
+        if let Some(val) = -value {
+            println!("{:?}", val);
+        }
+    }
+
+    #[test]
+    fn test_not() {
+        let string_value = Value::String(String::from(""));
+        let num = Value::Number(0.0);
+        let bl = Value::Bool(true);
+        let n = Value::None;
+        println!("{:?}", !n);
+    }
+}
